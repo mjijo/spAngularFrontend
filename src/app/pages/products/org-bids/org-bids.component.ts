6ac641d3f516442b5c2f7a2455bfac1c4be2164a
mjijo <96546469+mjijo@users.noreply.github.com>
2022-12-22 16:25:33 +0300

    updated the organisation bids

--- src/app/pages/products/org-bids/org-bids.component.ts
+++ src/app/pages/products/org-bids/org-bids.component.ts
@@ -11,6 +11,7 @@ export class OrgBidsComponent implements OnInit {
   constructor(private api : ApiService, private router: Router, private actRoute: ActivatedRoute) { }
   id:any;
   bidList:any;
+  
   ngOnInit(): void {
     this.actRoute.paramMap.subscribe(params => {
       this.id = params.get('id');
@@ -22,8 +23,20 @@ export class OrgBidsComponent implements OnInit {
   getBidsbyorg(id:any){
     this.api.getProductByOrgId(id).subscribe((data)=>{
       this.bidList = data;
-
-      console.log(this.bidList);
+      if( Array.isArray(data) ){
+        // loop through the data so we can access each object
+        data.forEach((bid:any) => {
+          bid.images = [];
+          // check for attachments in the product
+          if( 'attachments' in bid ){
+            // loop through the attachemnts object and get the image details to push to the images array
+            for (let property in bid.attachments) {
+              bid.images.push( {image_url: bid.attachments[property].original_url, name: bid.attachments[property].name} );
+            }
+          }
+        });
+      }
+      console.log(this.bidList.data);
     });
   }
 
